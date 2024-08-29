bd4454aa1f07774c9d9929a1434d7a36fe019950
Manoj Chathuranga <136339691+manojSDF@users.noreply.github.com>
2024-08-29 19:17:04 +0100

    Add files via upload
    Calculator

---
+++ Calculation Program/Calculation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number1");
+            int no1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter Number2");
+            int no2 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter Number3");
+            int no3 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter Number4");
+            int no4 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter Number5");
+            int no5 = Convert.ToInt16(Console.ReadLine());
+
+            int total = no1 + no2 + no3 + no4 + no5;
+            Console.WriteLine("The total is=  " + total);
+
+            Single avg = (Convert.ToSingle(no1) + Convert.ToSingle(no2) + Convert.ToSingle(no3) + Convert.ToSingle(no4) + Convert.ToSingle(no5)/5);
+            Console.WriteLine("The Averange is = " + avg);
+       
+
+            Console.ReadLine();
+        }
+    }
+}
